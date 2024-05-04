5abdb6939277453d13ed440dbc83de5210584329
MariaLvova <lvovamaria0202@gmail.com>
2024-05-04 17:10:00 +0200

    Pridajte používateľské slová ľubovoľnej veľkosti a pridajte ich do poľa slov

--- lexikograficky_nasledovnik_prepleteneho_slova/FileName.c
+++ lexikograficky_nasledovnik_prepleteneho_slova/FileName.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#pragma warning (disable:4996)
 
+
+void zadat_slovo(char** slovo) {
+    char temp[100];
+    scanf("%s", temp);
+    int size = strlen(temp);
+    *slovo = (char*)malloc((size + 1) * sizeof(char)); 
+    strcpy(*slovo, temp); 
+}
 
 int main() {
     char** slova;
@@ -10,4 +21,9 @@ int main() {
 
     slova = (char**)malloc((pocet_slov + 1) * sizeof(char*)); 
 
+    for (int i = 0; i < pocet_slov; i++) {
+        zadat_slovo(&slova[i]); 
+    }
+
+    slova[pocet_slov] = NULL;
 }
